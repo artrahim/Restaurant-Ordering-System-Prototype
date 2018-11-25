790c4d9fb89efac5860a649f6743b749128208aa
ucwillzhou <44952147+ucwillzhou@users.noreply.github.com>
2018-11-24 19:14:19 -0700

    Update Menu.xaml.cs

--- OrderingSystem/OrderingSystem/Menu.xaml.cs
+++ OrderingSystem/OrderingSystem/Menu.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,27 @@ namespace OrderingSystem
         public Starters()
         {
             InitializeComponent();
+        }
+        private void AddToOrder_Click_1(object sender, RoutedEventArgs e)
+        {
+            PopUpAddToOrder_1.IsOpen = true;
+        }
+
+        private void CancelButton_1_Click(object sender, RoutedEventArgs e)
+        {
+            PopUpAddToOrder_1.IsOpen = false;
+            naked.IsChecked = false;
+            medium.IsChecked = false;
+            mild.IsChecked = false;
+            hot.IsChecked = false;
+            bbq.IsChecked = false;
+            thai.IsChecked = false;
+            honey.IsChecked = false;
+            salt.IsChecked = false;
+            memphis.IsChecked = false;
+            blue.IsChecked = false;
+            chip.IsChecked = false;
+            ranch.IsChecked = false;
         }
     }
 }
